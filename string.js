c17a706eaf21ece3b1fe9f277793a47e1ef5e0ee
Kunter Grupmann <kunter.grupmann@voco.ee>
2023-09-08 13:23:01 +0300

    string operators

---
+++ string.js
@@ -0,0 +1,18 @@
+const firstname = "Kunter"
+const lastname = " Grupmann"
+let val = firstname + " " + lastname
+val = `${firstname} ${lastname}`
+
+val = lastname[0]
+val = lastname.charAt(1)
+val = lastname.charAt(lastname.lenght -2)
+val = lastnam.indexOf(`a`)
+val = lastname.lastIndexOf(`k`)
+
+val = lastname.substring(0, 4)
+val = lastname.slice(0, 4)
+val = lastname.slice(-3)
+
+const tags =`JS, HTMML, CSS`
+val = tags.split(`,`)
+console.log(val)
