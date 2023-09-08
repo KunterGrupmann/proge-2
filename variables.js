de41a7c7d2f74028dab78651960183d8427f5d45
Kunter Grupmann <kunter.grupmann@voco.ee>
2023-09-08 12:44:53 +0300

    Variables

---
+++ variables.js
@@ -0,0 +1,25 @@
+*// let and const
+/*
+let name = "Kunter"
+console.log(name)
+const surname = "Grupmann"
+surname = "Suur"
+*/
+/*
+// array
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+*/
+
+//objects
+const person = {
+	firstname: "Kunter"
+	lastname: " Grupmann"
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.lastname)
+person.department = "IT Akadeemia"
+console.log(person)
